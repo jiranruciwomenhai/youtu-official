7b03c5ef881345d4e3bab0f67ecca021c7d1c943
jiranruciwomenhai <jiranruciwomenhai@gmail.com>
2026-08-18 16:33:17 +0800

    Add Google Analytics tracking

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
+// Google Analytics 4
+(function () {
+  const measurementId = 'G-3E2PY6N88T';
+
+  window.dataLayer = window.dataLayer || [];
+
+  window.gtag = window.gtag || function () {
+    window.dataLayer.push(arguments);
+  };
+
+  window.gtag('js', new Date());
+  window.gtag('config', measurementId);
+
+  const script = document.createElement('script');
+  script.async = true;
+  script.src =
+    'https://www.googletagmanager.com/gtag/js?id=' +
+    encodeURIComponent(measurementId);
+
+  document.head.appendChild(script);
+})();
 document.documentElement.classList.add('js');
